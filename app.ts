bf064a8babee6f76c53610c7aa6d0e075e8ceaa7
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2018-02-12 14:41:32 -0800

    Transform javascript to typescript

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-const { Bot, MessageStyler, CardStyler } = require('botbuilder');
-const { BotFrameworkAdapter } = require('botbuilder-services');
-const restify = require('restify');
+import { Bot, MessageStyler, CardStyler } from 'botbuilder';
+import { BotFrameworkAdapter } from 'botbuilder-services';
+import *  as restify from 'restify';
 
-const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
+import { LuisRecognizer, QnAMaker, QnAMakerResult } from 'botbuilder-ai';
 
 const appId = '992b6593-cf27-4486-925d-8d6a732eb57c';
 const subscriptionKey = '833384c5334b48aa8b6843518fc32a46';
-const model = new LuisRecognizer(appId, subscriptionKey);
+let model: LuisRecognizer = new LuisRecognizer(appId, subscriptionKey);
 
 
 // Create server
@@ -16,7 +16,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create adapter and listen to servers '/api/messages' route.
-const adapter = new BotFrameworkAdapter({
+const adapter: BotFrameworkAdapter = new BotFrameworkAdapter({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
@@ -25,35 +25,45 @@ const adapter = new BotFrameworkAdapter({
 const qna = new QnAMaker({
     knowledgeBaseId: '0bda7583-c683-4b95-a8a5-d79166ad48c3',
     subscriptionKey: '640b8d79871a4b2382e99f71d25ef945',
-    top: 4
+    top: 4,
+    scoreThreshold: 0.5
 });
 
 
-server.post('/api/messages', adapter.listen());
+server.post('/api/messages', (adapter as any).listen());
 
 let luisConfidenceLevel = 0.5
 
 const bot = new Bot(adapter)
-    .onReceive((context) => {
+    .onReceive((context: BotContext) => {
         console.log('context', context);
         if (context.request.type === 'message') {
-            let greetingRegex = /^(hi|Hi|HI|hI|hello|Hello|HELLO|greetings|Greetings|good morning|Good morning|Good Morning|good afternoon|Good afternoon|Good Afternoon|hi |Hi |HI |hI |hello |Hello |HELLO |greetings |Greetings |good morning |Good morning |Good Morning |good afternoon |Good afternoon |Good Afternoon )$/
-            if (greetingRegex.test(context.request.text)) {
-                return context.reply(`Regex: Hello! How can I help you?`);
-            }
-            let helpRegex = /^(Help|help)$/;
-            if (helpRegex.test(context.request.text)) {
-                return context.reply(` 
-                List of commands can be run anytime        
-                  - Show my open orders
-                  - Show my closed orders
-                  - I forgot my password
-                        `);
+            let message = context.request.text || '';
+
+            let greetingRegexp = /^(hi|Hi|HI|hI|hello|Hello|HELLO|greetings|Greetings|good morning|Good morning|Good Morning|good afternoon|Good afternoon|Good Afternoon|hi |Hi |HI |hI |hello |Hello |HELLO |greetings |Greetings |good morning |Good morning |Good Morning |good afternoon |Good afternoon |Good Afternoon )$/
+            let agentRegexp = /^(Agent|agent|Connect to Agent)$/;
+            let helpRegexp = /^(Help|help)$/;
+
+            if (helpRegexp.test(message)) {
+                context.reply(` 
+    List of commands can be run anytime        
+      - Show my open orders
+      - Show my closed orders
+      - I forgot my password
+            `);
+                return;
             }
 
-            let agentRegex = /^(Agent|agent|Connect to Agent)$/;
-            if (agentRegex.test(context.request.text)) {
-                return context.reply(`Agent service is not available yet `);
+
+            if (greetingRegexp.test(message)) {
+                context.reply(`regexp: Hello! How can I help you?`);
+                return;
+            }
+
+
+            if (agentRegexp.test(message)) {
+                context.reply(`Agent service is not available yet `);
+                return;
             }
 
             return model.recognize(context)
@@ -61,22 +71,22 @@ const bot = new Bot(adapter)
 
                 .then((intent) => {
                     console.log(intent);
-                    if (intent.score > luisConfidenceLevel && intent.name != "None") {
+                    if (intent && intent.score > luisConfidenceLevel && intent.name != "None") {
                         switch (intent.name) {
                             case 'openOrders':
-                                return context.reply(processOpenOrderIntent());
+                                context.reply(processOpenOrderIntent());
                                 break;
                             case 'closedOrders':
-                                return context.reply(processClosedOrderIntent());
+                                context.reply(processClosedOrderIntent());
                                 break;
                             default:
-                                return context.reply(`Luis. Can't process intent: ${intent.name}`)
+                                context.reply(`Luis. Can't process intent: ${intent.name}`)
                         }
                     }
                     else {
                         const utterance = context.request.text || '';
                         return qna.getAnswers(utterance)
-                            .then((results) => {
+                            .then((results: QnAMakerResult[]) => {
                                 console.log(results);
                                 if (results && results.length > 0 && results[0].score > 0.5) {
                                     context.reply("QnA: " + results[0].answer);
@@ -98,13 +108,20 @@ const bot = new Bot(adapter)
     });
 
 
+interface Order {
+    id: string;
+    name: string;
+    orderDate: string;
+    dueDate?: string;
+    closedDate?: string;
+}
 
-var openOrders = {
+var openOrders: { [index: string]: Order } = {
     5107319: { "id": "5107319", "name": "Administration Account Request", "orderDate": "09/14/2017", "dueDate": "09/18/2017" },
     4972166: { "id": "4972166", "name": "Server Hosting - dmzServer (virtual) - New", "orderDate": "08/17/2017", "dueDate": "08/21/2017" }
 };
 
-var closedOrders = {
+var closedOrders: { [index: string]: Order } = {
     5112795: { "id": "5112795", "name": "Email Request", "orderDate": "09/16/2017", "closedDate": "09/18/2017" },
     4905025: { "id": "4905025", "name": "Mobile Device Management, Mobile PIM", "orderDate": "08/07/2017", "closedDate": "08/07/2017" },
     4453340: { "id": "4453340", "name": "Cloud Workplace - New", "orderDate": "03/23/2017", "closedDate": "03/24/2017" }
@@ -113,7 +130,7 @@ var closedOrders = {
 function processOpenOrderIntent() {
     var orders = getOpenOrderS();
     let response = '';
-    for (i in orders) {
+    for (let i in orders) {
         response += `Order #${orders[i].id}: 
               
         "${orders[i].name}" 
@@ -127,7 +144,7 @@ function processOpenOrderIntent() {
 function processClosedOrderIntent() {
     var orders = getClosedOrders();
     let response = '';
-    for (i in orders) {
+    for (let i in orders) {
         response += `Order #${orders[i].id}: 
               
         "${orders[i].name}" 
@@ -142,12 +159,12 @@ function getOpenOrderS() {
     var ordersArray = Object.keys(openOrders);
     var ordersToReturn = [];
 
-    for (k in ordersArray) {
+    for (let k in ordersArray) {
         ordersToReturn.push(openOrders[ordersArray[k]])
     }
     return ordersToReturn;
 }
-function getOpenOrder(id) {
+function getOpenOrder(id: string) {
     return openOrders[id];
 }
 
@@ -155,16 +172,16 @@ function getClosedOrders() {
     var ordersArray = Object.keys(closedOrders);
     var ordersToReturn = [];
 
-    for (k in ordersArray) {
+    for (let k in ordersArray) {
         ordersToReturn.push(closedOrders[ordersArray[k]])
     }
     return ordersToReturn;
 }
-function getClosedOrder(id) {
+function getClosedOrder(id: string) {
     return closedOrders[id];
 }
 
-function checkMyOrder(id) {
+function checkMyOrder(id: string) {
     id = id.toString();
     if (Object.keys(openOrders).indexOf(id) > -1) {
         return { "status": "open", "r": getOpenOrder(id) };
